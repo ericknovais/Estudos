858ec785a04bc959f437d71432cbbd5495daee47
ericknovais <erick.hora@Outlook.com>
2019-05-06 07:21:45 -0300

    Aula 05 - NameOf Operator

---
+++ Novidades C# 6.0/Nameof Operator/Nameof Operator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace Nameof_Operator
+{
+    public class Professor
+    {
+        public string Matricula { get; set; } = "0";
+        public string Nome { get; set; } = "Sem nome";
+        public DateTime Data { get; set; } = DateTime.Now;
+        public override string ToString()
+        {
+            return $"Matricula: {Matricula},Nome: {Nome}, Data: {Data}";
+        }
+    }
+
+    public static class Relatorio
+    {
+        public static void Imprimir(Professor prof)
+        {
+            WriteLine("Relatorio:");
+            WriteLine(prof?.ToString() ?? $"O parâmetro de nome " +
+                                               $"{nameof(prof)} não pode ser nulo");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Professor EN = null;
+            WriteLine(EN?.ToString() ?? $"Objeto {nameof(EN)} não instanciado");
+            Relatorio.Imprimir(EN);
+
+            EN = new Professor()
+            {
+                Matricula = "357",
+                Nome = "Erick Novais"
+            };
+
+            WriteLine(EN.ToString());
+            ReadLine();
+        }
+    }
+}
