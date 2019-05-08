0dca5862df8662579b0cec0ee16e3256d8499813
ericknovais <erick.hora@Outlook.com>
2019-05-08 07:06:42 -0300

    Aula 07 - Exceptions Filters

---
+++ Novidades C# 6.0/Exceptions Filters/Exceptions Filters/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace Exceptions_Filters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 0;
+            int r1 = 0;
+            int r2 = 0;
+
+            try
+            {
+                r1 = x / y;
+                WriteLine($"{x} dividido por {y} é igual a {r1}");
+
+                r2 = checked(x * y);
+                WriteLine($"{x} dividido por {y} é igual a {r2}");
+            }
+            //catch (DivideByZeroException e)
+            //{
+            //    WriteLine($"Mensagem: {e.Message}");
+            //}
+            //catch (OverflowException e)
+            //{
+            //    WriteLine($"Mensagem: {e.Message}");
+            //}
+            //catch (ArithmeticException e)
+            //{
+            //    WriteLine($"Erro aritimético: {e.Message}");
+            //}
+            catch (Exception e) when (e.Message.ToUpper().Contains("ESTOURO"))
+            {
+                WriteLine($"Mensagem: {e.Message}");
+            }
+            catch (Exception e) when (e.Message.ToUpper().Contains("ZERO"))
+            {
+                WriteLine($"Mensagem: {e.Message}");
+            }
+        }
+    }
+}
