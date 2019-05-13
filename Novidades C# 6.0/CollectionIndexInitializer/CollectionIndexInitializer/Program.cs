0828ef5a2e870b2a214e8a56952f5dded430b83c
ericknovais <erick.hora@Outlook.com>
2019-05-13 06:48:40 -0300

    Aula 08 - Dictionary Initializer

---
+++ Novidades C# 6.0/CollectionIndexInitializer/CollectionIndexInitializer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace CollectionIndexInitializer
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Alunos = new Dictionary<int, Aluno>()
+            {
+                [1] = new Aluno() { Nome = "Erick", Sobrenome = "Novais" },
+                [2]= new Aluno() { Nome = "Giovanna", Sobrenome = "Memoli" }
+            };
+
+            WriteLine("Informe a chave");
+            int index = Convert.ToInt32(ReadLine());
+            WriteLine($"Aluno: {Alunos[index].Nome} {Alunos[index].Sobrenome}");
+            ReadLine();
+        }
+    }
+}
