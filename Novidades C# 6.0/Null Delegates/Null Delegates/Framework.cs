4fe751606c1bd78eac97d43f3b8d85ddb510da69
ericknovais <erick.hora@Outlook.com>
2019-05-15 07:08:04 -0300

    Aula 11 - Null Delegates

---
+++ Novidades C# 6.0/Null Delegates/Null Delegates/Framework.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace Null_Delegates
+{
+    //public class Boleto
+    //{
+    //    public void Pagar(double valor)
+    //    {
+    //        WriteLine($"Pago valor de {valor} no boleto");
+    //    }
+    //}
+
+    public delegate void PagarEvent(double valor);
+
+    public class Pedido
+    {
+        //Boleto boleto = new Boleto();
+
+        public event PagarEvent Pagar;
+        public void Fechar(double valor)
+        {
+            //Delegação com delegate / event
+            Pagar?.Invoke(valor);
+        }
+    }
+}
---
+++ Novidades C# 6.0/Null Delegates/Null Delegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+using static System.Convert;
+
+namespace Null_Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Informe valor do pedido:");
+            var valor =ToDouble(ReadLine());
+            var pedido = new Pedido();
+            pedido.Pagar += p => WriteLine($"Pago valor de {p} no boleto");
+            pedido.Fechar(valor);
+        }
+
+        //private static void Pedido_Pagar(double valor)
+        //{
+        //    WriteLine($"Pago valor de {valor} no boleto");
+        //}
+    }
+}
