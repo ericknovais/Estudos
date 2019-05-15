bb03bff27751df0d71efa7a6d06122f52bd69948
ericknovais <erick.hora@Outlook.com>
2019-05-15 07:36:41 -0300

    Aula 12 - Gerendo classes de Exceptions

---
+++ Novidades C# 6.0/Classes de Exceptions/Classes de Exceptions/Conta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_de_Exceptions
+{
+    public class Conta
+    {
+        private const double LIMITE = -500; 
+        private double _saldo = 0;
+        public void Deposito(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor > _saldo + LIMITE)
+                throw new SaldoInsuficienteExpecion("Saldo insuficiente");
+           _saldo -= valor;
+        }
+
+        public double GetSaldo()
+        {
+            return _saldo;
+        }
+    }
+
+    [Serializable]
+    public class SaldoInsuficienteExpecion : Exception
+    {
+        public SaldoInsuficienteExpecion()
+        {
+        }
+
+        public SaldoInsuficienteExpecion(string message) : base(message)
+        {
+        }
+
+        public SaldoInsuficienteExpecion(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected SaldoInsuficienteExpecion(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Novidades C# 6.0/Classes de Exceptions/Classes de Exceptions/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+using static System.Convert;
+
+namespace Classes_de_Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Informe valor a depositar:");
+            var v1 = ToDouble(ReadLine());
+            WriteLine("Informe valor a sacar:");
+            var v2 = ToDouble(ReadLine());
+            var c = new Conta();
+            c.Deposito(v1);
+            c.Sacar(v2);
+            WriteLine($"Seu saldo é {c.GetSaldo()}");
+            ReadLine();
+        }
+    }
+}
