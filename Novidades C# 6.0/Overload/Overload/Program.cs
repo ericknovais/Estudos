604222abf64c99c9ceff4802485547413b81126d
ericknovais <erick.hora@Outlook.com>
2019-05-15 06:44:10 -0300

    Aula 10 - Overload

---
+++ Novidades C# 6.0/Overload/Overload/Program.cs
@@ -0,0 +1,69 @@
+﻿using static System.Console;
+using System;
+
+namespace Overload
+{
+    public class Aluno
+    {
+        public int Matricula { get; set; } = 0;
+        public string Nome { get; set; } = "Erick";
+        public override string ToString()
+        {
+            return $"Matricula: {Matricula} --" +
+                   $" Nome: {Nome}";
+        }
+    }
+
+    public class AlunoEspecial : Aluno
+    {
+        public string Situacao { get; set; } = "Especial";
+        public override string ToString()
+        {
+            return $"Matricula: {Matricula} --" +
+                   $" Nome: {Nome} -- Situação: {Situacao}";
+        }
+    }
+
+    class Program
+    {
+        static void Mostrar(string obj)
+        {
+            WriteLine($"Motrar(string): {obj}");
+        }
+
+        static void Mostrar(int obj)
+        {
+            WriteLine($"Motrar(int): {obj}");
+        }
+
+        static void Mostrar(bool obj)
+        {
+            WriteLine($"Motrar(bool): {obj}");
+        }
+
+        static void Mostrar(object obj)
+        {
+            WriteLine($"Motrar(object): {obj}");
+        }
+
+        static void Mostrar(Aluno obj)
+        {
+            WriteLine($"Motrar(Aluno): {obj}");
+        }
+
+        static void Mostrar(AlunoEspecial obj)
+        {
+            WriteLine($"Motrar(AlunoEspecial): {obj}");
+        }
+
+        static void Main(string[] args)
+        {
+            Mostrar("Olá Mundo");
+            Mostrar(10);
+            Mostrar(true);
+            Mostrar(new Aluno());
+            Mostrar(new AlunoEspecial() { Matricula = 123, Nome = "Erick Novais" });
+            ReadLine();
+        }
+    }
+}
