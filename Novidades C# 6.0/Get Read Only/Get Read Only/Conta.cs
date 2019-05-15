f2674a2eae4f17f7500d5eba7e654a50f5186d59
ericknovais <erick.hora@Outlook.com>
2019-05-15 19:58:10 -0300

    Aula 13 - Get Read-Only

---
+++ Novidades C# 6.0/Get Read Only/Get Read Only/Conta.cs
@@ -0,0 +1,26 @@
+﻿namespace Get_Read_Only
+{
+    public class Conta
+    {
+        // Antes do C# 6.0
+        /// public double Saldo { get; private set; } = 1000;
+        // Agora do C# 6.0
+        private double _saldo; // armazena 
+        public double Saldo { get { return _saldo; } } // mapeamento
+        public string Cliente { get; set; }
+        public void Sacar(double valor)
+        {
+            _saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        //public double GetSaldo()
+        //{
+        //    return Saldo;
+        //}
+    }
+}
---
+++ Novidades C# 6.0/Get Read Only/Get Read Only/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+namespace Get_Read_Only
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Conta() { Cliente = "Erick Novais" };
+            c.Depositar(500);
+            WriteLine($"Prezado {c.Cliente}, seu saldo é {c.Saldo}");
+            ReadLine();
+        }
+
+    }
+}
