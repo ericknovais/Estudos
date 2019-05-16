8e16c409cdc7562d790e109b6f36f8860a248a40
ericknovais <erick.hora@Outlook.com>
2019-05-15 21:07:14 -0300

    Aula 14 - Delegate Initializer

---
+++ Novidades C# 6.0/Event Delegate Initializer/Event Delegate Initializer/Framework.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace Event_Delegate_Initializer
+{
+    //public delegate void PagarEvent(double valor);
+    public class Pedido
+    {
+        //Boleto boleto = new Boleto();
+        // public event PagarEvent Pagar;
+        public string Cliente { get; set; }
+        public EventHandler<EventArgs> Pagar { get; set; }
+
+        public void Fechar(double valor)
+        {
+            //delegação com delegate / event
+            Pagar(this, new EventArgs());
+        }
+    }
+
+    //public class Boleto
+    //{
+    //    private void Pagar(double valor)
+    //    {
+    //        WriteLine($"Pago valor de {valor} no boleto");
+    //    }
+    //}
+}
---
+++ Novidades C# 6.0/Event Delegate Initializer/Event Delegate Initializer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace Event_Delegate_Initializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Informe valor do pedido");
+            var valor = ToDouble(ReadLine());
+            var pedido = new Pedido()
+            {
+                Cliente = "Erick",
+                Pagar = (s, e) => { WriteLine("Pago no Boleto"); }
+            };
+            pedido.Fechar(valor);
+            ReadLine();
+        }
+
+        //private static void Pedido_Pagar(object sender, EventArgs e)
+        //{
+        //    WriteLine($"Pago no boleto");
+        //}
+    }
+}
