a3373dc2b15944f85ac9c81719d0c2fc7d73d92e
ericknovais <erick.hora@Outlook.com>
2019-05-15 21:42:08 -0300

    Aula 15 - Lambda Debugging

---
+++ Novidades C# 6.0/Lambda Debugging/Lambda Debugging/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+using static System.Convert;
+
+namespace Lambda_Debugging
+{
+    public class Aluno
+    {
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public double Mensalidade { get; set; }
+    }
+
+    public static class Repositorio
+    {
+        public static List<Aluno> GetAlunos()
+        {
+            var lista = new List<Aluno>();
+            lista.Add(new Aluno() { Matricula = 1, Nome = "Erick", Mensalidade = 454 });
+            lista.Add(new Aluno() { Matricula = 2, Nome = "Caio", Mensalidade = 500 });
+            lista.Add(new Aluno() { Matricula = 3, Nome = "Leonardo", Mensalidade = 500 });
+            lista.Add(new Aluno() { Matricula = 4, Nome = "Paulo", Mensalidade = 50 });
+            return lista;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matricula = ToInt32(ReadLine());
+            var alunos = Repositorio.GetAlunos();
+            var query = alunos.Where(a => a.Matricula == matricula);
+            var aluno = query.FirstOrDefault(); 
+            WriteLine($"O aluno de Matricula {aluno.Matricula} se chama {aluno.Nome}");
+            ReadLine();
+        }
+    }
+}
