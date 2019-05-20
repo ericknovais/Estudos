6d87a4194e310f38f93a342457fcbbb768c5b0c4
ericknovais <erick.hora@Outlook.com>
2019-05-20 07:10:55 -0300

    Aula 05 - Inserindo uma mensagem  na página dinamicamente

---
+++ Front-end/Primeiros Passos com a Jquery/feedback/js/thumbs.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    const btn_thumb = $('#thumbs');
+    const contator = $('#thumbs .contador span');
+
+    btn_thumb.on('click', onClick);
+
+    function onClick() {
+        if (btn_thumb.hasClass('disable')) {
+            return;
+        }
+
+        const contagem = parseInt(contator.text());
+        contator.text(contagem + 1);
+
+        btn_thumb.addClass('disable');
+
+        const p = $('<p>', {
+            text: 'Obrigado pelo seu feedback!'
+        });
+
+        btn_thumb.after(p);
+    };
+});
