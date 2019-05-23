c4ebb4209b3c99c158a32390d426ca526ebc60aa
ericknovais <erick.hora@Outlook.com>
2019-05-23 07:36:26 -0300

    Aula 04 - Assessores e Propriedades

---
+++ Back-end/2 - C# Avançado/EstruturaCSharp/EstruturaCSharp/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaCSharp
+{
+    public class PosicaoClass
+    {
+        public int x;
+        public int y;
+        public override string ToString()
+        {
+            return $"X - {x.ToString()} Y - {y.ToString()}";
+        }
+    }
+
+    public struct PosicaoStruct
+    {
+        public int x;
+        public int y;
+        public override string ToString()
+        {
+            return $"X - {x.ToString()} Y - {y.ToString()}";
+        }
+    }
+
+    public enum Role
+    {
+        Arquiteto,
+        Developer,
+        Tester
+    }
+
+    public enum Color
+    {
+        Vermelo = 0,
+        Verde = 1,
+        Azul = 2
+    }
+
+    public enum FormaDePagamento
+    {
+        Boleto,
+        Cartao,
+        Cheque
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PosicaoClass pos1 = new PosicaoClass();
+            pos1.x = 10;
+            pos1.y = 20;
+
+            PosicaoStruct pos2 = new PosicaoStruct(); // ValeuType
+            pos2.x = 30;
+            pos2.y = 40;
+
+            PosicaoStruct pos3 = pos2; //Copia
+            pos3.y = 50;
+
+            FormaDePagamento formaPagto = FormaDePagamento.Boleto;
+            Role role = Role.Arquiteto;
+            Color cor = Color.Azul;
+
+            Console.WriteLine(pos1);
+            Console.WriteLine(pos2.ToString());
+            Console.WriteLine(pos3.ToString());
+            Console.WriteLine(formaPagto);
+            Console.WriteLine(role);
+            Console.WriteLine(cor);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Back-end/2 - C# Avançado/EstruturaCSharp/EstruturaCSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EstruturaCSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EstruturaCSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("973da095-bb68-409b-acee-6265e14ca950")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
