c4811e55a758e7e14a2bc8bf73de1edf90f99947
ericknovais <erick.hora@Outlook.com>
2019-05-24 07:22:16 -0300

    Aula 08 - Propriedades Automáticas

---
+++ Back-end/2 - C# Avançado/Propriedades Automaticas/Propriedades Automaticas/Geral.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propriedades_Automaticas
+{
+    public class Funcionario
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario()
+        {
+            this.Codigo = 0;
+            this.Nome = "Sem Nome";
+            this.Salario = 0;
+        }
+
+        public Funcionario(int codigo, string nome, double salario)
+        {
+            this.Codigo = codigo;
+            this.Nome = nome;
+            this.Salario = salario;
+        }
+    }
+}
---
+++ Back-end/2 - C# Avançado/Propriedades Automaticas/Propriedades Automaticas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propriedades_Automaticas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            funcionario.Codigo = 555;
+            funcionario.Nome = "Erick Novais";
+            funcionario.Salario = 2200;
+        }
+    }
+}
