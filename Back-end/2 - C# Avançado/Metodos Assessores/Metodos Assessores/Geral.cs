ec20dfef35af932960b2167c13703ad6145b0410
ericknovais <erick.hora@Outlook.com>
2019-05-23 23:56:37 -0300

    Aula 06 - Métodos Assessores

---
+++ Back-end/2 - C# Avançado/Metodos Assessores/Metodos Assessores/Geral.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos_Assessores
+{
+    public class Conta
+    {
+        private string _cliente;
+        private double _valor;
+
+        public Conta(string nomeCliente)
+        {
+            this._cliente = nomeCliente;
+        }
+
+        public Conta()
+        {
+
+        }
+
+        public void Sacar(double valor)
+        {
+            this._valor -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            this._valor += valor;
+        }
+
+        public double getValor()
+        {
+            return this._valor;
+        }
+
+        public string getCliente()
+        {
+            return this._cliente;
+        }
+
+        public void setCliente(string value)
+        {
+            this._cliente = value;
+        }
+    }
+}
---
+++ Back-end/2 - C# Avançado/Metodos Assessores/Metodos Assessores/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos_Assessores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+            conta.setCliente("Erick Novais");
+            conta.Depositar(1000);
+            conta.Sacar(200);
+            Console.WriteLine($"Cliente:`{conta.getCliente()}");
+            Console.WriteLine($"Saldo na data {DateTime.Now} é de {conta.getValor()}");
+            Console.ReadLine();
+        }
+    }
+}
