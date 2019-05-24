d171039d504950cff2f3a2ff12f01a50646e4b4d
ericknovais <erick.hora@Outlook.com>
2019-05-24 07:05:01 -0300

    Aula 07 - Assessores e propriedades

---
+++ Back-end/2 - C# Avançado/Assessores  e Propriedades/Assessores  e Propriedades/Geral.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessores__e_Propriedades
+{
+    public class Aluno
+    {
+        private int _matricula;
+        private string _nome;
+        private double _mensalidade;
+        private TipoAluno _tipo;
+
+        public int Matricula
+        {
+            get { return this._matricula; }
+            set { this._matricula = value; }
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        public double Mensalidade
+        {
+            get { return _mensalidade; }
+            set
+            {
+                if (value < 0)
+                    throw new MesalidadeNegativaException("Valor da mensalidade não pode ser negativo");
+                else
+                    _mensalidade = value;
+            }
+        }
+
+        public TipoAluno Tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; }
+        }
+    }
+
+    public enum TipoAluno
+    {
+        Regular,
+        Especial
+    }
+
+    public class MesalidadeNegativaException : Exception
+    {
+        public MesalidadeNegativaException(string mensagem) : base(mensagem)
+        { }
+    }
+}
---
+++ Back-end/2 - C# Avançado/Assessores  e Propriedades/Assessores  e Propriedades/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessores__e_Propriedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            
+            try
+            {
+                aluno.Matricula = 123;
+                aluno.Nome = "Erick";
+                aluno.Mensalidade = -454;
+                aluno.Tipo = TipoAluno.Regular;
+            }
+            catch (MesalidadeNegativaException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
