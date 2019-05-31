94d38c6de0ca4f5d9562c8714f6198a5d414718d
ericknovais <erick.hora@Outlook.com>
2019-05-30 22:56:35 -0300

    Aula 10 - Tipos de Inferências

---
+++ Back-end/2 - C# Avançado/TypeInference/TypeInference/Geral.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeInference
+{
+    public class Connection
+    {
+        public string ConnectionString { get; set; }
+        public void Conectar()
+        {
+            Console.WriteLine("Conectando");
+        }
+
+        public void Fechar()
+        {
+            Console.WriteLine("Fechando");
+        }
+    }
+
+    public static class Fabrica
+    {
+        public static Connection CriarConnection()
+        {
+            return new Connection();
+        }
+    }
+}
---
+++ Back-end/2 - C# Avançado/TypeInference/TypeInference/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeInference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InferenciaTipo();
+        }
+
+        private static void Dinamico()
+        {
+            // Palavar rezervada dynamic quando você não sabe, qual é o tipo do que sera 
+            // retornado, mas a aplicação só ira saber qual o tipo em tempo de execução 
+            // da aplicação
+            dynamic con = Fabrica.CriarConnection();
+            con.Teste = "Banco";
+        }
+
+        private static void InferenciaTipo()
+        {
+            // Palavra rezervada var para quando você não sabe, qual é o tipo do que sera
+            // retornado, então o compilador da um jeito de saber qual é esse tipo e 
+            // atribua o tipo a variavel isso em tempo de compilação
+            var connection = new Connection() { ConnectionString = "BANCO" };
+            connection.Conectar();
+            connection.Fechar();
+            Console.ReadKey();
+        }
+    }
+}
