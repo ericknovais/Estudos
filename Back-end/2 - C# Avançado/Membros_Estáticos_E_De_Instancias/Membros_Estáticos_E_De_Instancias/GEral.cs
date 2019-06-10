10ed8cc38223c19a0aac75b103af53fffa9e83bf
ericknovais <erick.hora@Outlook.com>
2019-06-10 05:58:27 -0300

    Aula 11 - Classes - Membros Estáticos e de Instancias

---
+++ Back-end/2 - C# Avançado/Membros_Estáticos_E_De_Instancias/Membros_Estáticos_E_De_Instancias/GEral.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Membros_Estáticos_E_De_Instancias
+{
+    public static class ConversorStatic
+    {
+        public static double CelsiusToFah(double temperatura)
+        {
+            return (temperatura * 9 / 5) + 32;
+        }
+
+        public static double FahToCelsius(double temperatura)
+        {
+            return (temperatura * -32) + 9 / 5;
+        }
+    }
+
+    public class ConversorInstancia
+    {
+        public static double temperatura;
+
+        public double CelsiusToFah()
+        {
+            return (temperatura * 9 / 5) + 32;
+        }
+
+        public double FahToCelsius()
+        {
+            return (temperatura * -32) + 9 / 5;
+        }
+    }
+}
---
+++ Back-end/2 - C# Avançado/Membros_Estáticos_E_De_Instancias/Membros_Estáticos_E_De_Instancias/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Membros_Estáticos_E_De_Instancias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conv1 = new ConversorInstancia();
+            var conv2 = new ConversorInstancia();
+
+            ConversorInstancia.temperatura = 30;
+            var celsius = conv1.CelsiusToFah();
+            //ConversorInstancia.temperatura = 70;
+            var fan = conv2.FahToCelsius();
+
+            Console.WriteLine($"celsius: {celsius}");
+            Console.WriteLine($"fan: {fan}");
+            Console.ReadLine();
+        }
+
+        private static void TesteStaticClass()
+        {
+            var temperatura = 35.0;
+            temperatura = ConversorStatic.CelsiusToFah(temperatura);
+            Console.WriteLine(temperatura);
+        }
+    }
+}
