6ec464eacbb1d635c8056223cc996f0815e6ad55
ericknovais <erick.hora@Outlook.com>
2019-06-10 21:03:49 -0300

    Aula 14 - Classes Genericas

---
+++ Back-end/2 - C# Avançado/Classes Genericas/Classes Genericas/Pinha.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Genericas
+{
+    public class Pinha<T>
+    {
+        int pos = 0;
+        T[] itens = new T[100];
+        public void Push(T item)
+        {
+            itens[pos] = item;
+            pos++;
+        }
+
+        public T Pop()
+        {
+            pos--;
+            return itens[pos];
+        }
+    }
+
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ Back-end/2 - C# Avançado/Classes Genericas/Classes Genericas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Genericas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pinha1 = new Pinha<string>();
+            pinha1.Push("Teste2");
+            pinha1.Push("Teste");
+            var pinha2 = new Pinha<int>();
+            pinha2.Push(10);
+            pinha2.Push(20);
+            var pinha3 = new Pinha<Cliente>();
+            pinha3.Push(new Cliente { Id = 1, Nome = "Erick Novais" });
+            pinha3.Push(new Cliente { Id = 2, Nome = "Giovanna" });
+            Cliente cliente = pinha3.Pop();
+
+        }
+    }
+}
