efc6c1373ca08017f53461c446fa076b68d8e781
ericknovais <erick.hora@Outlook.com>
2019-06-11 22:29:27 -0300

    Aula 17 - Sobrecarga De Métodos

---
+++ Back-end/2 - C# Avançado/SobrecargaDeMetodos/SobrecargaDeMetodos/CaixaDialogo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaDeMetodos
+{
+    public class CaixaDialogo
+    {
+        // default
+        public void Show(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+        }
+
+        public void Show(string mensagem, double delay)
+        {
+            Show(mensagem); // default
+        }
+
+        public void Show(string mensagem, int delay, bool ok, bool no)
+        {
+            Show(mensagem); // default
+        }
+    }
+}
---
+++ Back-end/2 - C# Avançado/SobrecargaDeMetodos/SobrecargaDeMetodos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaDeMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caixaDialogo = new CaixaDialogo();
+            caixaDialogo.Show(mensagem: "Confirmar?");
+            caixaDialogo.Show(mensagem: "Excluir?", delay: 10.0);
+            caixaDialogo.Show(mensagem: "Alterar?", delay: 10, ok: true, no: true);
+            Console.ReadLine();
+        }
+    }
+}
