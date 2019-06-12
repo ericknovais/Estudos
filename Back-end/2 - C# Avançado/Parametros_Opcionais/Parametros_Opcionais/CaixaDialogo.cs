2ab11c5172c2ee358aaf2ec2e5574de0383f1bcb
ericknovais <erick.hora@Outlook.com>
2019-06-11 22:00:25 -0300

    Aula 17 - Parametros Opcionais

---
+++ Back-end/2 - C# Avançado/Parametros_Opcionais/Parametros_Opcionais/CaixaDialogo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parametros_Opcionais
+{
+    public class CaixaDialogo
+    {
+        public void Show(string mensagem, bool ok, bool no, bool cancel = false, int delay = 10)
+        {
+            Console.WriteLine(mensagem);
+        }
+    }
+}
---
+++ Back-end/2 - C# Avançado/Parametros_Opcionais/Parametros_Opcionais/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parametros_Opcionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caixaDialogo = new CaixaDialogo();
+            caixaDialogo.Show(mensagem: "Confirmar?", ok: true, no: true);
+
+
+            var caixaDialogo2 = new CaixaDialogo();
+            caixaDialogo2.Show(mensagem: "Excluir?", ok: true, no: true);
+            Console.ReadLine();
+        }
+    }
+}
