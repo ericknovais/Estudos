78dcc249b08b6bdd6c4baec628b33881e86dd0ac
ericknovais <erick.hora@Outlook.com>
2019-06-11 21:22:47 -0300

    Aula 15 - Parâmetros Nomeados

---
+++ Back-end/2 - C# Avançado/ParametrosNomeados/ParametrosNomeados/Dialogo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametrosNomeados
+{
+    public class DialogoBad
+    {
+        public void Show(string mensagem, bool mostrarBotaoOk,
+                         bool mostrarBotaoNo, bool mostrarBotaoCancel, int delay)
+        {
+            Console.WriteLine(mensagem);
+        }
+    }
+
+    public class Dialogo
+    {
+        public void Show(DialogoParams parametros)
+        {
+            Console.WriteLine(parametros.Mensagem);
+        }
+    }
+
+    public class DialogoParams
+    {
+        public string Mensagem;
+        public bool MostrarBotaoOk;
+        public bool MostrarBotaoNo;
+        public bool MostrarBotaoCancel;
+        public int Delay;
+    }
+}
---
+++ Back-end/2 - C# Avançado/ParametrosNomeados/ParametrosNomeados/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametrosNomeados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dlg = new DialogoBad();
+            dlg.Show(mensagem: "Confirmar Operação?", mostrarBotaoOk: true,
+                     mostrarBotaoNo: true, mostrarBotaoCancel: false, delay: 10);
+
+            var parametros = new DialogoParams()
+            {
+                Mensagem = "Confirmar Operação?",
+                MostrarBotaoOk = true,
+                MostrarBotaoNo = true,
+                MostrarBotaoCancel = false,
+                Delay = 10
+            };
+
+            var dlg2 = new Dialogo();
+            dlg2.Show(parametros: parametros);
+            Console.ReadLine();
+        }
+    }
+}
