e615274f829b5086702522c60db406a42345c227
ericknovais <erick.hora@Outlook.com>
2019-06-11 22:58:29 -0300

    Aula 18 - Propriedades Indexadas

---
+++ Back-end/2 - C# Avançado/PropriedadesIndexadas/PropriedadesIndexadas/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropriedadesIndexadas
+{
+    public class Produto
+    {
+        private List<string> _itens;
+        public Produto()
+        {
+            this._itens = new List<string>();
+        }
+
+        public void Add(string item)
+        {
+            this._itens.Add(item);
+        }
+
+        public string Get(int index)
+        {
+            return this._itens[index];
+        }
+
+        //Propriedade indexada
+        public string this[int index]
+        {
+            get
+            {
+                // return Get(index); // Mesma coisa para os 2 
+                return this._itens[index]; 
+            }
+        }
+
+        public int this[string nome]
+        {
+            get
+            {
+                return this._itens.IndexOf(nome);
+            }
+        }
+    }
+}
---
+++ Back-end/2 - C# Avançado/PropriedadesIndexadas/PropriedadesIndexadas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropriedadesIndexadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var produtos = new Produto();
+            produtos.Add("Iphone X Plus");
+            produtos.Add("Moto E 7 Plus");
+            produtos.Add("Sansung JS9");
+
+            Console.WriteLine(produtos[0]);
+            Console.WriteLine(produtos[1]);
+            Console.WriteLine(produtos[2]);
+            Console.WriteLine($"A posição do item iPad é {produtos["Iphone X Plus"]}");
+            Console.ReadKey();
+        }
+    }
+}
