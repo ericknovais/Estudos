7d2af7378471f3d0199ceb0fa30643a0a5a6d2ab
ericknovais <erick.hora@Outlook.com>
2019-06-17 06:47:57 -0300

    Aula 19 - Sobrecarga de Método (Override)

---
+++ Back-end/2 - C# Avançado/Sobrecarga_Metodos_Override/Sobrecarga_Metodos_Override/Arquivos.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobrecarga_Metodos_Override
+{
+    public class Musica
+    {
+        public string Nome;
+        public int Tamanho;
+        public void Abrir()
+        {
+            Console.WriteLine($"Tocando música {Nome}");
+        }
+    }
+
+    public class DocumentoWord
+    {
+        public string Nome;
+        public int Tamanho;
+        public void Abrir()
+        {
+            Console.WriteLine($"Mostrando documento do Word: {Nome}");
+        }
+    }
+
+    public class Imagem
+    {
+        public string Nome;
+        public int Tamanho;
+        public void Abrir()
+        {
+            Console.WriteLine($"Exibindo imagem: {Nome}");
+        }
+    }
+
+    public class Windows
+    {
+        public ArrayList _arquivos;
+
+        public Windows()
+        {
+            this._arquivos = new ArrayList();
+        }
+
+        public void Add(Object arquivo)
+        {
+            this._arquivos.Add(arquivo);
+        }
+
+        public void Abrir(Object arquivo)
+        {
+            if (arquivo is Musica)
+                (arquivo as Musica).Abrir();
+            if (arquivo is DocumentoWord)
+                (arquivo as DocumentoWord).Abrir();
+            if (arquivo is Imagem)
+                (arquivo as Imagem).Abrir();
+        }
+    }
+}
---
+++ Back-end/2 - C# Avançado/Sobrecarga_Metodos_Override/Sobrecarga_Metodos_Override/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobrecarga_Metodos_Override
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musica mp3 = new Musica { Nome = "This is Amarica - Gambino" };
+            DocumentoWord doc = new DocumentoWord() { Nome = ".net Magazine - Artigo POO" };
+            Imagem img = new Imagem() { Nome = "Aguia" };
+            Windows windows = new Windows();
+
+            windows.Add(mp3);
+            windows.Add(doc);
+            windows.Add(img);
+
+            windows.Abrir(mp3);
+            windows.Abrir(doc);
+            windows.Abrir(img);
+
+            Console.ReadLine();
+        }
+    }
+}
