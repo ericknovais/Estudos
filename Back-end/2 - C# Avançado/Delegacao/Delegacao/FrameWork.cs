476465355ef94374c9f30742cc4467311ff97d45
ericknovais <erick.hora@Outlook.com>
2019-06-18 06:51:40 -0300

    Aula 21 - Entendendo o processo de Delegação em C#

---
+++ Back-end/2 - C# Avançado/Delegacao/Delegacao/FrameWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegacao
+{
+    public class Boleto
+    {
+        public void Pagar(double valor)
+        {
+            Console.WriteLine($"Boleto pago no valor de {valor.ToString("c")}");
+        }
+    }
+
+    public class Pedido
+    {
+        private Boleto _boleto = new Boleto();
+
+        public void Fechar(double valor)
+        {
+            //delegação...
+            this._boleto.Pagar(valor);
+        }
+    }
+}
---
+++ Back-end/2 - C# Avançado/Delegacao/Delegacao/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pedido = new Pedido();
+            pedido.Fechar(valor: 2000);
+            Console.ReadLine();
+        }
+    }
+}
