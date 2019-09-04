fadea673696f1a83c1f4dcf07ea9e555021bcd11
ericknovais <erick.hora@Outlook.com>
2019-09-04 07:22:48 -0300

    Aula 24 - Definindo Delegates

--- Back-end/2 - C# Avançado/Delegacao/Delegacao/FrameWork.cs
+++ Back-end/2 - C# Avançado/Delegacao/Delegacao/FrameWork.cs
@@ -6,22 +6,16 @@ using System.Threading.Tasks;
 
 namespace Delegacao
 {
-    public class Boleto
-    {
-        public void Pagar(double valor)
-        {
-            Console.WriteLine($"Boleto pago no valor de {valor.ToString("c")}");
-        }
-    }
+    public delegate void PagarEvent(double valor);
 
     public class Pedido
     {
-        private Boleto _boleto = new Boleto();
+        public event PagarEvent Pagar;
 
         public void Fechar(double valor)
         {
             //delegação...
-            this._boleto.Pagar(valor);
+            this.Pagar(valor);
         }
     }
 }
